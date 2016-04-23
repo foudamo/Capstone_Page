12aadde7d2c420f7a9832e98079e096ddef818e2
Alfred Lam <alfredlam93@gmail.com>
2016-04-23 17:17:54 -0400

    let's try again..

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,40 @@
 <?php
+	if (isset($_POST["submit"])) {
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$message = $_POST['message'];
+		$from = 'Contact Form'; 
+		$to = 'ballpickingrobot@gmail.com'; 
+		$subject = "Website Contact Form: $name ";
+		
+		$body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+		// Check if name has been entered
+		if (!$_POST['name']) {
+			$errName = 'Please enter your name';
+		}
+		
+		// Check if email has been entered and is valid
+		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+			$errEmail = 'Please enter a valid email address';
+		}
+		
+		//Check if message has been entered
+		if (!$_POST['message']) {
+			$errMessage = 'Please enter your message';
+		}
+		
+		// If there are no errors, send the email
+		if (!$errName && !$errEmail && !$errMessage) {
+			if (mail ($to, $subject, $body, $from)) {
+			$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+			} else {
+			$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
+			}
+		}
+	}
+	
+/*		
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
@@ -11,20 +47,21 @@ if(empty($_POST['name'])  		||
 	
 $name = $_POST['name'];
 $email_address = $_POST['email'];
-$message = $_POST['message'];
+$message = $_POST['message'];*/
 	
-// Create the email and send the message
-
 /* 
 email: ballpickingrobot@gmail.com
 password: ballboy1
 */
 
+/*
+// Create the email and send the message
 $to = 'ballpickingrobot@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: ballpickingrobot@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
-return true;			
+return true; 
+*/		
 ?>
